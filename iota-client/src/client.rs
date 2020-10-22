eaa1f4e42e9237e9ea6c559ca9896c98d628cee0
DyrellC <dyrell.chapman@iota.org>
2020-10-22 11:22:21 -0600

    Add timeout

--- iota-client/src/client.rs
+++ iota-client/src/client.rs
@@ -25,6 +25,7 @@ macro_rules! response {
             .header("Content-Type", "application/json")
             .header("X-IOTA-API-Version", "1")
             .body($body.to_string())
+            .timeout(std::time::Duration::from_secs(5))
             .send()
             .await?
             .json()
